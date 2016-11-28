4a7b7647e1c2bbd8c1227a8399272483c3cc77a4
Jesusbaby <olayemifolakemi@yahoo.com>
2016-11-28 15:48:43 +0100

    Update data-type files for testing

--- data-types/dataTypes.js
+++ data-types/dataTypes.js
@@ -1,30 +1,32 @@
-function dataTypes(data) {
-  if (typeof(data) === null || typeof(data) === undefined) {
-    return 'no value';
-  }
-  if (typeof(data) == 'string') {
-    return data.length;
-  } else if (typeof(data) == 'boolean') {
-    return data;
-  } else if (typeof(data) == 'number') {
+var app = {
+  dataTypes: function(data) {
+    if (typeof(data) === null || typeof(data) === undefined) {
+      return 'no value';
+    }
+    if (typeof(data) == 'string') {
+      return data.length;
+    } else if (typeof(data) == 'boolean') {
+      return data;
+    } else if (typeof(data) == 'number') {
 
-    if (data < 100) {
-      return "less than 100";
-    } else if (data === 100) {
-      return 'equal to 100';
-    } else {
-      return "more than 100";
+      if (data < 100) {
+        return "less than 100";
+      } else if (data === 100) {
+        return 'equal to 100';
+      } else {
+        return "more than 100";
+      }
     }
-  }
-  if (typeof(data) == 'object') {
-    if (data.length < 3) {
-      return 'undefined';
-    } else {
-      return data[2];
+    if (typeof(data) == 'object') {
+      if (data.length < 3) {
+        return 'undefined';
+      } else {
+        return data[2];
+      }
     }
-  }
-  if (typeof(data) == 'function') {
-    return dataTypes(true);
+    if (typeof(data) == 'function') {
+      return dataTypes(true);
+    }
   }
 }
-console.log(dataTypes("God is great"));
+module.exports = app
